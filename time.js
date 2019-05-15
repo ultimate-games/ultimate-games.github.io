3f74793c70cf2900c7e4ed4262f3f5319004157e
Brian Baldner <42284551+brianbaldner@users.noreply.github.com>
2019-05-14 20:47:10 -0700

    Update time.js

--- time.js
+++ time.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jun 13, 2019 2:10:00").getTime();
+var countDownDate = new Date("Jun 13, 2019 14:10:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
