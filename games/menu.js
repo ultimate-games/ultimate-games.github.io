bc0d32afdc1ff44fc49bd84cf0c941178c3cd540
Brian Baldner <brianbaldner@users.noreply.github.com>
2019-05-30 21:34:01 -0700

    Update

--- games/menu.js
+++ games/menu.js
@@ -1,1 +1,8 @@
-
+  var x = document.getElementsByClassName("topnav");
+  x.innerHTML = "<a class='logo'>UG</a>
+  <a target='_parent' class='link' href='/#'>Home</a>
+  <a target='_parent' class='active link' href='/games'>Games</a>
+    <a target='_parent' class='link' href='/gba'>GBA Emulator</a>
+    <a target='_parent' class='link' href='/gba'>GBA Emulator</a>
+    <a target='_parent' class='link' href='/shop'>Shop</a>
+  <a class='title'><script id='_waunxu'>var _wau = _wau || []; _wau.push(['dynamic', 've5ro51iw0', 'nxu', 'c4302bffffff', 'small']);</script><script async src='//waust.at/d.js'></script></a>";
